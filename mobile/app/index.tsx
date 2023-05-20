6a16fa0fb237719375ffe0bb4c8664e1d50bd2fc
Jeison Tomio <medriase@gmail.com>
2023-05-19 22:58:45 -0300

    completed class 4

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,23 +1,14 @@
-import { StatusBar } from 'expo-status-bar'
-import { Text, ImageBackground, View, TouchableOpacity } from 'react-native'
+import { Text, View, TouchableOpacity } from 'react-native'
 import { useRouter } from 'expo-router'
 import { useAuthRequest, makeRedirectUri } from 'expo-auth-session'
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
 import * as SecureStore from 'expo-secure-store'
 
-import blurBg from '../src/assets/bg-blur.png'
-import Stripes from '../src/assets/stripes.svg'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
-import { styled } from 'nativewind'
-import { useEffect } from 'react'
+
+import React, { useEffect } from 'react'
 import { api } from '../src/lib/api'
 
-const StyledStripes = styled(Stripes)
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
   tokenEndpoint: 'https://github.com/login/oauth/access_token',
@@ -27,12 +18,6 @@ const discovery = {
 
 export default function App() {
   const router = useRouter()
-
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
 
   const [, response, signInWithGithub] = useAuthRequest(
     {
@@ -55,33 +40,22 @@ export default function App() {
   }
 
   useEffect(() => {
-    console.log(
-      makeRedirectUri({
-        scheme: 'nlwspacetimer',
-      }),
-    )
+    // console.log(
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetimer',
+    //   }),
+    // )
 
     if (response?.type === 'success') {
       const { code } = response.params
 
       handleGithubOAuthCode(code)
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [response])
 
-  console.log(response)
-
-  if (!hasLoadedFonts) {
-    return null
-  }
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center justify-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 items-center justify-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
         <View className="space-y-2">
@@ -106,7 +80,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   )
 }
